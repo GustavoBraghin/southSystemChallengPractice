51de768ecb1afce16b629c1ec8f539ae4efd837a
GustavoBraghin <gugabraghin@gmail.com>
2022-07-05 16:14:20 -0300

    feat: created eventModel

---
+++ southSystemDesafio/Model/eventModel.swift
@@ -0,0 +1,19 @@
+//
+//  eventModel.swift
+//  southSystemDesafio
+//
+//  Created by Gustavo da Silva Braghin on 05/07/22.
+//
+
+import Foundation
+
+struct eventModel: Decodable {
+    let date: Double
+    let description: String
+    let image: String
+    let longitude: Double
+    let latitude: Double
+    let price: Float
+    let title: String
+    let id: String
+}
