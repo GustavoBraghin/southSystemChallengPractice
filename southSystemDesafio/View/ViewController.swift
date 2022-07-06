a61c62ab14586b4348b368c7e3d596a6b9ab9633
GustavoBraghin <gugabraghin@gmail.com>
2022-07-06 15:29:27 -0300

    feat: created eventsViewModel, update code from view to viewModel

--- southSystemDesafio/View/ViewController.swift
+++ southSystemDesafio/View/ViewController.swift
@@ -8,34 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var events: [EventModel]? = []
+    var eventsViewModel = EventsViewModel()
+    
     lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .plain)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.rowHeight = 120
+        tableView.rowHeight = 90
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
         return tableView
     }()
     
-    var events: [eventModel] = []
-    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        configureTableView()
         
-        apiRequest().fetchData { events, error in
-            self.events = events!
+        configureView()
+        eventsViewModel.loadData { events, error in
+            self.events = events
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
         }
+        navigationController?.title = "Events"
     }
     
-    func configureTableView() {
-        tableView.backgroundColor = .cyan
+    func configureView() {
+        tableView.backgroundColor = .black
         view.backgroundColor = .red
         view.addSubview(tableView)
         NSLayoutConstraint.activate([
@@ -47,13 +47,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return events.count
+        return events?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
-        cell.textLabel?.text = "\(events[indexPath.row].title)"
-        print(cell.textLabel!.text!)
+        cell.textLabel?.text = "\(events?[indexPath.row].title ?? "error")"
         return cell
     }
 }
